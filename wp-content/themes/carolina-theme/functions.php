59c8d28f32556a1f5d3714cc50d2c78a46d072a7
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-06-29 18:42:49 -0500

    example of changing elements order in theme

---
+++ wp-content/themes/carolina-theme/functions.php
@@ -0,0 +1,4 @@
+<?php
+// changing price order in product view
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 1);

ed84604152075f0801a670d2fad20907269c79cf
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-06-30 18:16:04 -0500

    Currency updates

--- wp-content/themes/carolina-theme/functions.php
+++ wp-content/themes/carolina-theme/functions.php
@@ -9,3 +9,11 @@ add_filter('loop_shop_per_page', 'products_per_page');
 function products_per_page($columns) {
     return 3;
 }
+
+// change to mexican pesos (MXN)
+add_filter('woocommerce_currency_symbol', 'carolinaspa_mxn', 10, 2);
+
+function carolinaspa_mxn($symbol, $coin) {
+    $symbol = "MXN $";
+    return $symbol;
+}

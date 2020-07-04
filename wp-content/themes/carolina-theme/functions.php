b60c720f3d79bc6b18aa29e4d1b4ca8386b7119a
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-07-04 14:32:34 -0500

    change description tab text for product name

--- wp-content/themes/carolina-theme/functions.php
+++ wp-content/themes/carolina-theme/functions.php
@@ -111,3 +111,19 @@ function spa_saved_money($price, $product) {
     return $price;
 }
 add_filter('woocommerce_get_price_html', 'spa_saved_money', 10, 2);
+
+// change tab text for product name
+function spa_change_tab_text($tabs) {
+    global $post;
+    if(isset($tabs['description']['title'])) {
+        $tabs['description']['title'] = $post->post_title;
+    }
+    return $tabs;
+}
+add_filter('woocommerce_product_tabs', 'spa_change_tab_text', 10, 1);
+
+function spa_change_title_tab($title) {
+    global $post;
+    return $post->post_title;
+}
+add_filter('woocommerce_product_description_heading', 'spa_change_title_tab', 10, 1);
